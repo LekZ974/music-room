6b13723d608fa79d77ab03950e2d0c149083fb6a
Alexandre Hoareau <alexandrehoareau@MacBook-Pro-de-Alexandre.local>
2019-02-26 15:47:27 +0100

    feature: fix Dockerfile for react app

--- react-front/App.js
+++ react-front/App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>React app for Music-Room!</Text>
+        <Text>React app for Music-Room!!!</Text>
       </View>
     );
   }
