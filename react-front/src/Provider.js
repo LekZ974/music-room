52eb95f2ab4029a0cf64808cfc5358465f2dc6c7
LekZ974 <ahoareau@student.42.fr>
2019-03-02 00:46:53 +0100

    add redux persist (#7)

--- react-front/src/Provider.js
+++ react-front/src/Provider.js
@@ -1,14 +1,18 @@
 import React from 'react';
 
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { AppLoading } from 'expo';
+import store, { persistor } from './redux/store';
 
 import Main from './Main';
 import { alert } from './redux/actions/alert';
 
 const WrapperProvider = () => (
   <Provider store={store}>
-    <Main />
+    <PersistGate loading={<AppLoading />} persistor={persistor}>
+      <Main />
+    </PersistGate>
   </Provider>
 );
 
--- react-front/src/redux/store.js
+++ react-front/src/redux/store.js
@@ -1,13 +1,23 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { persistStore, persistReducer } from 'redux-persist';
 import thunk from 'redux-thunk';
-
-import reducer from './reducers/reducer';
+import storage from 'redux-persist/lib/storage';
+import rootReducer from './reducers/reducer';
 
 const middlewares = [thunk];
 
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const composeFn = __DEV__ ? composeWithDevTools : compose;
 
-const store = composeFn(applyMiddleware(...middlewares))(createStore)(reducer);
+const store = composeFn(applyMiddleware(...middlewares))(createStore)(persistedReducer);
+
+export const persistor = persistStore(store);
 
 export default store;
