c020dd935f34118abe90d0dedd70aaab9c53d7d7
Alexandre Hoareau <alexandrehoareau@MacBook-Pro-de-Alexandre.local>
2019-03-07 09:33:16 +0100

    react-front: fix header

--- react-front/src/components/Header.js
+++ react-front/src/components/Header.js
@@ -3,6 +3,6 @@ import React from 'react';
 import { Header as NBHeader } from 'native-base';
 
 const Header = props => {
-  return <NBHeader style={{ backgroundColor: 'blue' }} {...props} />;
+  return <NBHeader {...props} />;
 };
 export default Header;
